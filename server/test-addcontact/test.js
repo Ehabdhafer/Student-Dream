8e638a4425cb911f964f1958d4af81a70fd5dbca
Ehabdhafer <ehabzaferabed@gmail.com>
2023-11-20 22:04:28 +0300

    edit files

--- server/test-addcontact/test.js
+++ server/test-addcontact/test.js
@@ -31,15 +31,48 @@ describe("addContactMessage", () => {
     });
 
     await addContactMessage(req, res);
-
-    // expect(res.json()).toBe({
-    //   name: "issadw",
-    //   email: "issahaddadhaddad@gmail.com",
-    //   message: "hhi jesti",
-    //   user: "65584f4d4a6ff886a9efc38b",
-    //   senderType: "user",
-    // });
-
     newMessageSaveMock.mockRestore();
+  });
+});
+
+describe("ALL", () => {
+  describe("no name", () => {
+    test.only("should check the presence of name", () => {
+      const contactUsInstance = new ContactUs({
+        name: "issa haddad",
+      });
+
+      expect(contactUsInstance.name).toBe("issa haddad");
+    });
+  });
+
+  describe("no email", () => {
+    test.only("should check the presence of name", () => {
+      const contactUsInstance = new ContactUs({
+        email: "john@gmail.com",
+      });
+
+      expect(contactUsInstance.email).toBe("john@gmail.com");
+    });
+  });
+
+  describe("no massege", () => {
+    test.only("should check the presence of name", () => {
+      const contactUsInstance = new ContactUs({
+        message: "Hello, this is a test message.",
+      });
+
+      expect(contactUsInstance.message).toBe("Hello, this is a test message.");
+    });
+  });
+
+  describe("no senderType", () => {
+    test.only("should check the presence of name", () => {
+      const contactUsInstance = new ContactUs({
+        senderType: "student",
+      });
+
+      expect(contactUsInstance.senderType).toBe("student");
+    });
   });
 });
